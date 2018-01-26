2f820abec499da63db61731943ece9c526da1915
Konstantinos Vasilakos <konvas@Konstantinoss-iMac.local>
2018-01-26 20:26:30 +0300

    push changes

--- imports/specs.js
+++ imports/specs.js
@@ -1,13 +1,12 @@
 let scaleValue = require('scale-value');
 
 const specs = {
-    attack: scaleValue(0, 1, 100, 44100),
-    decay: scaleValue(0, 1, 100, 22050),
-    sustain: scaleValue(0, 1, 100, 44100),
-    release: scaleValue(0, 1, 1, 100),
-    gain: scaleValue(0, 1, 0.1, 0.8),
-    frequency: scaleValue(0, 1, 120.0, 1220.0),
-    triggerRelease: scaleValue(0, 1, 0.1, 10)
+    attack: scaleValue(0, 1, 0.1, 1.0 * 44100),
+    decay: scaleValue(0, 1, 0.1, 1.0 * 11025),
+    sustain: scaleValue(0, 1, 0.1, 1.0 * 44100),
+    release: scaleValue(0, 1, 0.1, 1.0 * 22050),
+    gain: scaleValue(0, 1, 0.1, 0.1, 1.0),
+    frequency: scaleValue(0, 1, 120.0, 1220.0)
 };
 
 export default specs;
