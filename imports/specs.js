c92eabbf573011868b0fb814aa02dd5b390c1bcd
Scott Wilson <i@scottwilson.ca>
2018-04-07 14:40:51 +0100

    Initial Multi-synth support
    - this also fixes some bugs regarding parameter scaling and sliders.
    - Apologies for the large commit, but this was a bit complicated with merging commits from out of sync branches
    
    Signed-off-by: Scott Wilson <i@scottwilson.ca>

--- imports/specs.js
+++ imports/specs.js
@@ -19,37 +19,6 @@ const specs = {
       	inmax = 1;
         return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
     },
-
-    attack: function(val, outmin, outmax){
-    	  inmin = 0.001;
-      	inmax = 1.0;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
-    decay: function(val, outmin, outmax){
-    	  inmin = 0.1;
-      	inmax = 1.0;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
-    sustain: function(val, outmin, outmax){
-    	  inmin = 0.0;
-      	inmax = 0.9;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
-    release: function(val, outmin, outmax){
-    	  inmin = 0.1;
-      	inmax = 3.0;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
-    detune: function(val, outmin, outmax){
-    	  inmin = -Math.PI;
-      	inmax = Math.PI;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
-    frequency: function(val, outmin, outmax){
-    	  inmin = 120.0;
-      	inmax = 1220.0;
-        return (((val - inmin) / (inmax - inmin)) * (outmax - outmin)) + outmin;
-    },
 };
 
 export default specs;
