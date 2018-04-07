0728d1b02f340c7c2403f2dc817208e330df120d
Scott Wilson <i@scottwilson.ca>
2018-04-07 17:21:17 +0100

    Use MIDI notes instead of freq for more human friendly pitch
    Signed-off-by: Scott Wilson <i@scottwilson.ca>

--- client/sliders.js
+++ client/sliders.js
@@ -20,15 +20,15 @@ Template.ipsosboard.rendered = function () {
     }).Link('lower').to('-inline-<div class="tooltip"></div>', sliderHandler)
         .Link('upper').to('-inline-<div class="tooltip"></div>', sliderHandler);
 
-    this.$("#frequency").noUiSlider({
-        start: Session.get('frequency'),
+    this.$("#midinote").noUiSlider({
+        start: Session.get('midinote'),
         orientation: "vertical",
         connect: true,
         step: 0.1,
         margin: 3000,
         range: {
-            'min': 20.0,
-            'max': 20000.0
+            'min': 0,
+            'max': 127
         }
     }).on('slide', function (ev, val) {
         Session.set(this.id, [val[0], val[1]]);
